231b0a96cb235854d6d648c21e68d949296bb889
Totenkaf <uscov.1998@mail.ru>
2022-09-19 00:13:49 +0300

    add checks

--- game/tic_tac_toe.py
+++ game/tic_tac_toe.py
@@ -1,8 +1,12 @@
-# Copyright 2022 by Artem Ustsov
+'''
+TicTacToe game project.
+Copyright 2022 by Artem Ustsov
+'''
 import sys
 
 
 class TicTacGame:
+    '''Main TicTacToe game Object'''
     def __init__(self, input_fd=sys.stdin):
         '''Initialize basic game settings'''
         self.board_size = 3
@@ -12,19 +16,27 @@ class TicTacGame:
 
     def show_board(self) -> None:
         '''Draw the game board as pseudo graph in stdout'''
+
         print("\n")
+
         print("\t     |     |")
-        print("\t  {}  |  {}  |  {}".format(self.board_field[0], self.board_field[1], self.board_field[2]))
+        print(f"\t  {self.board_field[0]}  |  "
+              f"{self.board_field[1]}  |  "
+              f"{self.board_field[2]}")
         print('\t_____|_____|_____')
 
         print("\t     |     |")
-        print("\t  {}  |  {}  |  {}".format(self.board_field[3], self.board_field[4], self.board_field[5]))
+        print(f"\t  {self.board_field[3]}  |  "
+              f"{self.board_field[4]}  |  "
+              f"{self.board_field[5]}")
         print('\t_____|_____|_____')
 
         print("\t     |     |")
-
-        print("\t  {}  |  {}  |  {}".format(self.board_field[6], self.board_field[7], self.board_field[8]))
+        print(f"\t  {self.board_field[6]}  |  "
+              f"{self.board_field[7]}  |  "
+              f"{self.board_field[8]}")
         print("\t     |     |")
+
         print("\n")
 
     def validate_input(self, current_player) -> tuple:
@@ -39,8 +51,7 @@ class TicTacGame:
                 print(f"{current_player} choose {player_chosen_pos}")
                 if player_chosen_pos == '':
                     return False, -1
-                else:
-                    player_chosen_pos = int(player_chosen_pos)
+                player_chosen_pos = int(player_chosen_pos)
             except ValueError:
                 print("Wrong input. Enter the number position from 1 to 9\n")
                 continue
@@ -51,9 +62,8 @@ class TicTacGame:
 
             if str(self.board_field[player_chosen_pos - 1]) not in "XO":
                 break
-            else:
-                print("The cell is occupied. Try again!\n")
-                continue
+            print("The cell is occupied. Try again!\n")
+            continue
 
         return True, player_chosen_pos
 
@@ -63,15 +73,15 @@ class TicTacGame:
         '''
         if step_counter % 2 == 0:
             current_player = "X"
-            is_validated, player_chosen_pos = self.validate_input(current_player)
-            if is_validated:
+            is_valid, player_chosen_pos = self.validate_input(current_player)
+            if is_valid:
                 self.board_field[player_chosen_pos - 1] = current_player
             else:
                 return "STOP"
         else:
             current_player = "O"
-            is_validated, player_chosen_pos = self.validate_input(current_player)
-            if is_validated:
+            is_valid, player_chosen_pos = self.validate_input(current_player)
+            if is_valid:
                 self.board_field[player_chosen_pos - 1] = current_player
             else:
                 return "STOP"
@@ -106,10 +116,12 @@ class TicTacGame:
                       (0, 1, 2), (3, 4, 5),
                       (6, 7, 8), (0, 3, 6),
                       (1, 4, 7), (2, 5, 8),
-                      (0, 4, 8), (2, 4, 6)
+                      (0, 4, 8), (2, 4, 6),
                      )
         for win_cord in win_cords:
-            if self.board_field[win_cord[0]] == self.board_field[win_cord[1]] == self.board_field[win_cord[2]]:
+            if self.board_field[win_cord[0]] ==\
+                    self.board_field[win_cord[1]] ==\
+                    self.board_field[win_cord[2]]:
                 print(current_player, "Wins!")
                 return True
         return False
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-# Copyright 2022 by Artem Ustsov
+'''
+TicTacToe game project. Main file
+Copyright 2022 by Artem Ustsov
+'''
 import sys
+
 from game.tic_tac_toe import TicTacGame
 
-
 if __name__ == "__main__":
-    # with open(sys.argv[1], 'r') if len(sys.argv) > 1 else sys.stdin as f:
-        f = sys.stdin
+    with open(sys.argv[1], 'r', encoding="utf-8") \
+            if len(sys.argv) > 1 else sys.stdin as f:
         game = TicTacGame(input_fd=f)
         game.start_pair_game()
