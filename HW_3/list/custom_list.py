ea74b06fcabc6e39a448abb70e7fcd810d827d44
Totenkaf <uscov.1998@mail.ru>
2022-09-29 23:27:39 +0300

    add class template. ci initialize. test initialize

---
+++ HW_3/list/custom_list.py
@@ -0,0 +1,36 @@
+"""
+IntList project.
+Copyright 2022 by Artem Ustsov
+"""
+import sys
+
+class CustomList(list):
+    def __init__(self):
+        pass
+
+    def __sub__(self, other):
+        pass
+
+    def __isub__(self, other):
+        pass
+
+    def __rsub__(self, other):
+        pass
+
+    def __add__(self, other):
+        pass
+
+    def __iadd__(self, other):
+        pass
+
+    def __radd__(self, other):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
---
+++ HW_3/list/main.py
@@ -0,0 +1,10 @@
+"""
+TicTacToe game project. Main file
+Copyright 2022 by Artem Ustsov
+"""
+import sys
+
+from list.custom_list import CustomList
+
+if __name__ == "__main__":
+    pass
