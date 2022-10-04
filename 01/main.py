60001521fb1ef5a755944e3480c06cc83b0e7a12
Totenkaf <uscov.1998@mail.ru>
2022-10-04 23:31:12 +0300

    refactor output. refactor project dir_name

---
+++ 01/main.py
@@ -0,0 +1,28 @@
+"""
+TicTacToe game project. Main file
+Copyright 2022 by Artem Ustsov
+"""
+import argparse
+import sys
+
+from game.tic_tac_toe import TicTacGame
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", default="stdin")
+    parser.add_argument("-o", "--output", default="stdout")
+    args = parser.parse_args()
+    print(args)
+
+    with open(
+        args.input,
+        "r",
+        encoding="utf-8",
+    ) if args.input != "stdin" else sys.stdin as input_file:
+        with open(
+            args.output,
+            "w",
+            encoding="utf-8",
+        ) if args.output != "stdout" else sys.stdout as output_file:
+            game = TicTacGame(input_fd=input_file, output_fd=output_file)
+            game.start_pair_game()
