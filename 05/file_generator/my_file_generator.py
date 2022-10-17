d897344940c3d61d6a4798eb92388bd5071402d0
Artem Ustsov <a.ustsov@vk.team>
2022-10-18 01:17:07 +0300

    file generator is done

--- 05/file_generator/my_file_generator.py
+++ 05/file_generator/my_file_generator.py
@@ -3,104 +3,96 @@ File generator package main file
 Copyright 2022 by Artem Ustsov
 """
 
-from typing import Any, NoReturn
+import argparse
+import sys
 import os
+from typing import List, NoReturn, Tuple
 
-
-# def chunkify(filename, size=1024*1024):
-#     file_end = os.path.getsize(filename)
-#     with open(filename, mode='r') as file:
-#         chunk_end = file.tell()
-#
-#         while True:
-#             chunk_start = chunk_end
-#
-#             #  set the shift from the current pointer position
-#             # file.seek(size, 1)
-#             file.read(size)
-#             chunk_end = file.tell()
-#             chunk_size = chunk_end - chunk_start
-#
-#             yield chunk_start, chunk_size
-#
-#             if chunk_end > file_end:
-#                 break
-#
-#
-def find_occurrences(line: list, occurrences_list: list):
-    for occurrences in occurrences_list:
-        if occurrences in line:
-            print(''.join(line))
-#
-#
-# def process_wrapper(chunk_start, chunk_size, filename, process_job, occurrences_list):
-#     with open(filename, 'r') as file:
-#         file.seek(chunk_start)
-#         # lines = file.read(chunk_size).splitlines()
-#         lines = file.read(chunk_size)
-#         for line in lines:
-#             process_job(line, occurrences_list)
-#
-#
 
 class Chunker:
-    """
+    """Chunker object for parsing"""
 
-    """
-    @classmethod
-    def chunkify(cls, fname, size=1024*1024):
+    def __init__(self, filename: str, output_fd, buffer_size: int = 1024*1024):
+        self.filename = filename
+        self.output_fd = output_fd
+        self.buffer_size = buffer_size
+
+    def chunkify(self) -> Tuple[int, int]:
+        """Make chunks.
+        Return tuple of chunk_start position and chunk_size in file to parse
         """
 
-        """
-        file_end = os.path.getsize(fname)
-        with open(fname, 'r') as f:
-            chunk_end = f.tell()
+        if self.buffer_size < 1:
+            raise ValueError("Buffer size must be strong positive")
+
+        with open(self.filename, 'rb') as file:
+            file_end = os.path.getsize(self.filename)
+            chunk_end = file.tell()
             while True:
                 chunk_start = chunk_end
-                f.read(size)
-                cls._EOC(f)
-                chunk_end = f.tell()
+                file.seek(self.buffer_size, 1)
 
-                yield chunk_start, chunk_end - chunk_start
+                self.find_end_of_chunk(file)
+
+                chunk_end = file.tell()
+                chunk_offset = chunk_end - chunk_start
+
+                yield chunk_start, chunk_offset
                 if chunk_end >= file_end:
                     break
 
     @staticmethod
-    def _EOC(f):
-        """
+    def find_end_of_chunk(file) -> NoReturn:
+        """Check the end of chunk with skipping incomplete line
+        Incomplete means that line doesn't end with \n"""
 
-        """
-        l = f.readline()  # incomplete line
-        p = f.tell()
-        l = f.readline()
-        while l and l[0] != '\n':
-            p = f.tell()
-            l = f.readline()
-        f.seek(p)  # revert one line
+        #  skip incomplete line
+        file.readline()
+        p = file.tell()
+        line = file.readline()
+
+        while line:
+            p = file.tell()
+            line = file.readline()
+
+        #  revert one line
+        file.seek(p)
+
+    def process_wrapper(self, chunk_start: int, size: int, search_values: List[str]) -> NoReturn:
+        """Takes the chunk_start position and size of the chunk (in bytes) and parse the line"""
+        with open(self.filename, 'r', encoding='utf-8') as input_file:
+            with open(self.output_fd, 'w', encoding='utf-8') \
+                    if self.output_fd != 'stdout' \
+                    else sys.stdout as output_file:
+
+                input_file.seek(chunk_start)
+                sentences = input_file.read(size).splitlines()
+                for sentence in sentences:
+                    sentence_with_occurrence = self.find_occurrence(sentence, search_values)
+                    print(*sentence_with_occurrence, file=output_file)
 
     @staticmethod
-    def read(fname, chunk):
-        """
-
-        """
-        with open(fname,'r') as f:
-            f.seek(chunk[0])
-            return f.read(chunk[1])
-
-    @staticmethod
-    def parse(chunk):
-        """
-
-        """
-        for line in chunk.splitlines():
-            yield line
+    def find_occurrence(sentence: str, search_values: List[str]) -> str:
+        """Tries to find exactly occurrence of the search value in sentence"""
+        for search_value in search_values:
+            #  set() for avoiding duplicate words in sentences
+            for word in set(sentence.split()):
+                if word == search_value:
+                    yield sentence
 
 
 if __name__ == '__main__':
-    filename_ = 'input.txt'
-    search_values = ["роза"]
-    chunker = Chunker()
-    for ch_st, ch_size in chunker.chunkify(filename_):
-        pass
-    # for chunk_start_, chunk_size_ in chunkify(filename_, size=48):
-    #     process_wrapper(chunk_start_, chunk_size_, filename_, find_occurrences, search_values)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", default="input.txt")
+    parser.add_argument("-o", "--output", default="stdout")
+    args = parser.parse_args()
+
+    occurrences_list = ["роза", "всегда"]
+
+    chunker = Chunker(args.input, args.output, buffer_size=32)
+
+    for chunk_start_pos, chunk_size in chunker.chunkify():
+        chunker.process_wrapper(chunk_start=chunk_start_pos,
+                                size=chunk_size,
+                                search_values=occurrences_list,
+                                )
