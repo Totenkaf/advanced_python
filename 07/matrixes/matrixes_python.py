8caf37ddcd630522ca883fd5a022c6aa624eab44
Artem Ustsov <a.ustsov@vk.team>
2022-11-01 01:42:51 +0300

    python matris is done

--- 07/matrixes/matrixes_python.py
+++ 07/matrixes/matrixes_python.py
@@ -4,102 +4,198 @@ Calculations on python
 Copyright 2022 by Artem Ustsov
 """
 
+# import argparse
+import logging
+
+# import os
+import random
 import sys
-from typing import List
-
-# Функция для поиска наиболее эффективного способа умножения
-# заданная последовательность матриц
-# Функция для поиска наиболее эффективного способа умножения
-# заданная последовательность матриц
-def matrixChainMultiplication(dims, i, j, lookup):
-    # Базовый случай: одна матрица
-    if j <= i + 1:
-        return 0
-
-    # хранит минимальное количество скалярных умножений (т.е. стоимость)
-    # требуется для вычисления матрицы `M[i+1] … M[j] = M[i…j]`
-    min = sys.maxsize
-
-    # , если подзадача видится впервые, решите ее и
-    # сохраняет результат в таблице поиска
-    if lookup[i][j] == 0:
-
-        # берут минимум по каждому возможному положению, в котором
-        # Последовательность матриц # может быть разделена
-
-        '''
-            (M[i+1]) × (M[i+2]………………M[j])
-            (M[i+1]M[i+2]) × (M[i+3…………M[j])
-            …
-            …
-            (M[i+1]M[i+2]…………M[j-1]) × (M[j])
-        '''
-
-        for k in range(i + 1, j):
-
-            # повторяется для `M[i+1]…M[k]`, чтобы получить матрицу `i × k`
-            cost = matrixChainMultiplication(dims, i, k, lookup)
-
-            # повторяется для `M[k+1]…M[j]`, чтобы получить матрицу `k × j`
-            cost += matrixChainMultiplication(dims, k, j, lookup)
-
-            # стоимость умножения двух матриц `i × k` и `k × j`
-            print(dims[i], dims[k], dims[j])
-            cost += dims[i] * dims[k] * dims[j]
-
-            if cost < min:
-                min = cost
-
-        lookup[i][j] = min
-        print(lookup)
-
-    # возвращает минимальную стоимость умножения `M[j+1]…M[j]`
-    return lookup[i][j]
+from typing import List, Optional
 
 
-# if __name__ == '__main__':
-#     # Матрица `M[i]` имеет размерность `dims[i-1] × dims[i]` для `i=1…n`
-#     # Вход #: матрица 10 × 30, матрица 30 × 5, матрица 5 × 60
-#     dims = [10, 30, 5, 60]
-#
-#     # Таблица поиска # для хранения решения уже вычисленных подзадач
-#     lookup = [[0 for x in range(len(dims))] for y in range(len(dims))]
-#
-#     n = len(dims)
-#     print('The minimum cost is', matrixChainMultiplication(dims, 0, n - 1, lookup))
+def fill_matrix(
+    num_rows: int,
+    num_cols: int,
+    randomize: bool,
+    input_fd,
+) -> List[List[int]]:
+    """
 
+    :param num_rows: Matrix number of rows
+    :param num_cols: Matrix number of columns
+    :param randomize: If true allow to fill with random numbers
+    :param input_fd: Input file descriptor
+    :return: Matrix of numbers
+    """
 
-def fill_matrix(rows: int, cols: int, input_fd=sys.stdin) -> List[List[int]]:
-    if not isinstance(rows, int) or not isinstance(cols, int):
+    if not isinstance(num_rows, int) or not isinstance(num_cols, int):
         raise ValueError
 
-    print(f'Fill maxtrix {rows} x {cols}:\n')
-    matrix = []
-
+    logging.getLogger().info("Fill matrix %i x %i:", num_rows, num_cols)
+    matrix = [[0 for _ in range(num_cols)] for _ in range(num_rows)]
     try:
-        for i in range(rows):
-            array = []
-            for j in range(cols):
-                array.append(int(input_fd.readline()))
-            matrix.append(array)
+        for i in range(num_rows):
+            for j in range(num_cols):
+                if randomize:
+                    matrix[i][j] = random.randint(0, 5)
+                else:
+                    str_value = input_fd.readline()
+                    if not str_value:
+                        break
+                    matrix[i][j] = int(str_value)
 
     except ValueError:
-        raise ValueError("Value in matrix must be a number")
-
+        logging.getLogger().debug("Value in matrix must be a number")
+    logging.getLogger().debug("Filled matrix %s", matrix)
     return matrix
 
 
-def fill_matrix_chain(matrix_chain_pattern: List[int]) -> List[List[List[int]]]:
+def fill_matrix_chain(
+    matrix_chain_pattern: List[int],
+    filler: Optional,
+    randomize: bool = False,
+    input_fd=sys.stdin,
+) -> List[List[List[int]]]:
+    """Fill the matrix with specific values
+
+    :param matrix_chain_pattern: list with matrix dimensions like:
+    [2, 5, 4, 3] means 2x5 5x4 4x3
+    :param filler: function to fill matrix
+    :param randomize: allow to produce random numbers
+    to fill the matrix
+    :param input_fd: input file descriptor
+    :return: filled matrix with number
+    """
+
     filled_matrix_chain = []
-    for num_rows, num_cols in zip(matrix_chain_pattern, matrix_chain_pattern[1:]):
-        filled_matrix_chain.append(fill_matrix(rows=num_rows, cols=num_cols))
+    for num_rows, num_cols in zip(
+        matrix_chain_pattern, matrix_chain_pattern[1:]
+    ):
+        if num_cols < 1 or num_rows < 1:
+            raise IndexError("Must be a strong positive")
+        filled_matrix_chain.append(
+            filler(
+                num_rows=num_rows,
+                num_cols=num_cols,
+                randomize=randomize,
+                input_fd=input_fd,
+            )
+        )
+        logging.getLogger().debug(
+            "Filled matrix chain %s", filled_matrix_chain
+        )
     return filled_matrix_chain
 
 
-# def multiply_matrix_chain(matrix_chain: List[List[int]]) -> List[List[int]]:
-#     result_matrix = [[0 for i in range(length)] for i in range(length)]
+def multiply_two_matrixes(matrix_first, matrix_second):
+    """Check matrix dimensions and multiply on by another
+
+    :param matrix_first: matrix with integers
+    :param matrix_second: matrix with integers
+    :return: result of two matrix multiplication
+    """
+    matrix_first_rows = len(matrix_first)
+    matrix_first_cols = len(matrix_first[0])
+    matrix_second_rows = len(matrix_second)
+    matrix_second_cols = len(matrix_second[0])
+
+    if matrix_first_cols != matrix_second_rows:
+        raise ValueError(
+            "First matrix row length should be equal "
+            "to second matrix col length"
+        )
+
+    result_matrix = [
+        [0 for _ in range(matrix_second_cols)]
+        for _ in range(matrix_first_rows)
+    ]
+    logging.getLogger().debug(
+        "Result matrix dimensions: %ix%i",
+        len(result_matrix),
+        len(result_matrix[0]),
+    )
+
+    for i in range(matrix_first_rows):
+        for j in range(matrix_second_cols):
+            for k in range(matrix_first_cols):
+                result_matrix[i][j] += (
+                    matrix_first[i][k] * matrix_second[k][j]
+                )
+
+    return result_matrix
 
 
-if __name__ == "__main__":
-    matrix_chain_pattern = [2, 2, 2]
-    print(fill_matrix_chain(matrix_chain_pattern))
+def multiply_matrix_chain(
+    matrix_chain_list: List[List[List[int]]],
+) -> List[List[int]]:
+    """Store result in stack and pop it for multiplication with another matrix
+
+    :param matrix_chain_list: list of matrix dimensions like [2, 3, 3]
+    :return: result of chain of matrix multiplication
+    """
+
+    try:
+        matrix_result_stack = [matrix_chain_list[0]]
+        for i in range(1, len(matrix_chain_list)):
+            result = multiply_two_matrixes(
+                matrix_result_stack.pop(), matrix_chain_list[i]
+            )
+            matrix_result_stack.append(result)
+    except ValueError as error:
+        raise error
+    else:
+        return matrix_result_stack.pop()
+
+
+# if __name__ == "__main__":
+#     if not os.path.isdir("../logs"):
+#         os.mkdir("../logs")
+#
+#     FORMAT_LOG = "%(asctime)s: %(message)s"
+#     file_log = logging.FileHandler("logs/matrix.log")
+#     console_out = logging.StreamHandler()
+#
+#     logging.basicConfig(
+#         handlers=(file_log, console_out),
+#         format=FORMAT_LOG,
+#         level=logging.INFO,
+#         datefmt="%H:%M:%S",
+#     )
+#     logging.getLogger().info("=====PROGRAM START=====")
+#
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("-c", "--chain", default="2,4,3,1")
+#     parser.add_argument("-r", "--random", default="True")
+#
+#     args = parser.parse_args()
+#
+#     try:
+#         matrix_chain_pattern = list(map(int, args.chain.split(",")))
+#     except ValueError(
+#         "Matrix chain pattern should be a integer number"
+#     ) as error:
+#         logging.getLogger().info(error)
+#     else:
+#         try:
+#             if args.random == "False":
+#                 matrix_chain_list = fill_matrix_chain(
+#                     matrix_chain_pattern, fill_matrix
+#                 )
+#             else:
+#                 matrix_chain_list = fill_matrix_chain(
+#                     matrix_chain_pattern, fill_matrix, randomize=True
+#                 )
+#             result_matrix = multiply_matrix_chain(matrix_chain_list)
+#         except ValueError as error:
+#             logging.getLogger().info(error)
+#         except IndexError as error:
+#             logging.getLogger().info(error)
+#         else:
+#             logging.getLogger().info(
+#                 "Result matrix shape: %ix%i",
+#                 len(result_matrix),
+#                 len(result_matrix[0]),
+#             )
+#             logging.getLogger().info("Result matrix")
+#             logging.getLogger().info(result_matrix)
+#     logging.getLogger().info("=====PROGRAM STOP=====")
---
+++ 07/matrixes/tests/unit/matrixes_python_unit_test.py
@@ -0,0 +1,247 @@
+"""
+Matrix chain miltiplication.
+Calculations on python unit test
+Copyright 2022 by Artem Ustsov
+"""
+
+import os
+import sys
+import unittest
+
+from matrixes.matrixes_python import (
+    fill_matrix,
+    fill_matrix_chain,
+    multiply_matrix_chain,
+    multiply_two_matrixes,
+)
+
+
+class TestClient(unittest.TestCase):
+    """
+    Main test class for my_client module
+    """
+
+    def setUp(self) -> None:
+        """Prepare ENV
+        :return: None
+        """
+
+        self.matrix_chain_pattern = [3, 2, 3]
+        self.matrix_2x2 = [[1, 2], [3, 4]]
+
+        self.result_matrix_2x2 = [[7, 10], [15, 22]]
+
+        self.matrix_3x2 = [[1, 2], [3, 4], [5, 6]]
+
+        self.matrix_2x4 = [[1, 2, 3, 4], [5, 6, 7, 8]]
+
+        self.result_matrix_3x4 = [
+            [11, 14, 17, 20],
+            [23, 30, 37, 44],
+            [35, 46, 57, 68],
+        ]
+
+        # 3x2 2x2 2x4
+        self.result_matrix_3x2x4 = [
+            [57, 74, 91, 108],
+            [125, 162, 199, 236],
+            [193, 250, 307, 364],
+        ]
+
+        self.input_2x2_matrix_filename = "input_2x2_matrix.txt"
+        self.input_3x2_matrix_filename = "input_3x2_matrix.txt"
+        self.input_2x4_matrix_filename = "input_2x4_matrix.txt"
+
+        if not os.path.isdir(self.input_2x2_matrix_filename):
+            with open(
+                self.input_2x2_matrix_filename, "w", encoding="utf-8"
+            ) as file:
+                for i in range(len(self.matrix_2x2)):
+                    for j in range(len(self.matrix_2x2[0])):
+                        file.write(str(self.matrix_2x2[i][j]) + "\n")
+
+        if not os.path.isdir(self.input_3x2_matrix_filename):
+            with open(
+                self.input_3x2_matrix_filename, "w", encoding="utf-8"
+            ) as file:
+                for i in range(len(self.matrix_3x2)):
+                    for j in range(len(self.matrix_3x2[0])):
+                        file.write(str(self.matrix_3x2[i][j]) + "\n")
+
+        if not os.path.isdir(self.input_2x4_matrix_filename):
+            with open(
+                self.input_2x4_matrix_filename, "w", encoding="utf-8"
+            ) as file:
+                for i in range(len(self.matrix_2x4)):
+                    for j in range(len(self.matrix_2x4[0])):
+                        file.writelines(str(self.matrix_2x4[i][j]) + "\n")
+
+    def tearDown(self) -> None:
+        """Clear ENV
+
+        :return: None
+        """
+
+        try:
+            os.remove(self.input_2x2_matrix_filename)
+            os.remove(self.input_3x2_matrix_filename)
+            os.remove(self.input_2x4_matrix_filename)
+        except OSError as why:
+            print(why)
+
+    def test_fill_matrix(self) -> None:
+        """Test filling via file
+        Test filling via random
+
+        :return: None
+        """
+
+        with open(
+            self.input_2x2_matrix_filename, "r", encoding="utf-8"
+        ) as file:
+            self.matrix = fill_matrix(
+                num_rows=len(self.matrix_2x2),
+                num_cols=len(self.matrix_2x2[0]),
+                input_fd=file,
+                randomize=False,
+            )
+
+            for i in range(len(self.matrix)):
+                for j in range(len(self.matrix[0])):
+                    self.assertEqual(self.matrix[i][j], self.matrix_2x2[i][j])
+
+            self.random_matrix = fill_matrix(
+                num_rows=len(self.matrix_2x2),
+                num_cols=len(self.matrix_2x2[0]),
+                input_fd=file,
+                randomize=True,
+            )
+
+            self.assertEqual(len(self.random_matrix), len(self.matrix_2x2))
+            self.assertEqual(
+                len(self.random_matrix[0]), len(self.matrix_2x2[0])
+            )
+
+            self.assertRaises(
+                ValueError, fill_matrix, "1", 1, sys.stdin, False
+            )
+            self.assertRaises(
+                ValueError, fill_matrix, 1, [1], sys.stdin, False
+            )
+
+    def test_fill_matrix_chain(self) -> None:
+        """Fill matrix chain
+
+        :return: None
+        """
+
+        with open(
+            self.input_3x2_matrix_filename, "r", encoding="utf-8"
+        ) as file:
+            filled_matrix_chain = fill_matrix_chain(
+                matrix_chain_pattern=[3, 2],
+                filler=fill_matrix,
+                randomize=False,
+                input_fd=file,
+            )
+
+            self.assertEqual(len(filled_matrix_chain), 1)
+            for i in range(len(filled_matrix_chain[0])):
+                for j in range(len(filled_matrix_chain[0][0])):
+                    self.assertEqual(
+                        filled_matrix_chain[0][i][j], self.matrix_3x2[i][j]
+                    )
+
+    def test_fill_matrix_chain_wrong_dim(self) -> None:
+        """Test wrong matrix_hain_patter dim
+
+        :return: None
+        """
+
+        self.assertRaises(
+            IndexError, fill_matrix_chain, [2, 0, 2], fill_matrix
+        )
+        self.assertRaises(IndexError, fill_matrix_chain, [1, -1], fill_matrix)
+
+    def test_multiply_two_matrixes(self) -> None:
+        """Test multiplying
+
+        :return: None
+        """
+
+        result_quadratic_matrix = multiply_two_matrixes(
+            self.matrix_2x2, self.matrix_2x2
+        )
+        for i in range(len(result_quadratic_matrix)):
+            for j in range(len(result_quadratic_matrix[0])):
+                self.assertEqual(
+                    result_quadratic_matrix[i][j],
+                    self.result_matrix_2x2[i][j],
+                )
+
+        self.assertEqual(
+            len(result_quadratic_matrix), len(self.result_matrix_2x2)
+        )
+        self.assertEqual(
+            len(result_quadratic_matrix[0]), len(self.result_matrix_2x2[0])
+        )
+
+        result_rectangle_matrix = multiply_two_matrixes(
+            self.matrix_3x2, self.matrix_2x4
+        )
+        for i in range(len(result_rectangle_matrix)):
+            for j in range(len(result_rectangle_matrix[0])):
+                self.assertEqual(
+                    result_rectangle_matrix[i][j],
+                    self.result_matrix_3x4[i][j],
+                )
+
+        self.assertEqual(
+            len(result_rectangle_matrix), len(self.result_matrix_3x4)
+        )
+        self.assertEqual(
+            len(result_rectangle_matrix[0]), len(self.result_matrix_3x4[0])
+        )
+
+    def test_multiply_two_matrixes_wrong_dim(self) -> None:
+        """Test wron multiplying
+
+        :return: None
+        """
+
+        self.assertRaises(
+            ValueError,
+            multiply_two_matrixes,
+            self.matrix_2x2,
+            self.matrix_3x2,
+        )
+        self.assertRaises(
+            ValueError,
+            multiply_two_matrixes,
+            self.matrix_3x2,
+            self.matrix_3x2,
+        )
+
+    def test_multiply_matrix_chain(self) -> None:
+        """Test multiplying chain of matrixes
+
+        :return: None
+        """
+
+        matrix_chain_list = [
+            self.matrix_3x2,
+            self.matrix_2x2,
+            self.matrix_2x4,
+        ]
+        result_matrix = multiply_matrix_chain(matrix_chain_list)
+
+        self.assertEqual(len(result_matrix), len(self.result_matrix_3x2x4))
+        self.assertEqual(
+            len(result_matrix[0]), len(self.result_matrix_3x2x4[0])
+        )
+
+        for i in range(len(result_matrix)):
+            for j in range(len(result_matrix[0])):
+                self.assertEqual(
+                    result_matrix[i][j], self.result_matrix_3x2x4[i][j]
+                )
