212e1de36cadf77c05b7d6fe68267fba1646f301
Artem Ustsov <a.ustsov@vk.team>
2022-11-04 15:40:10 +0300

    add init

--- 07/matrixes/tests/perf/pert_test.py
+++ 07/matrixes/tests/perf/pert_test.py
@@ -4,27 +4,27 @@ Calculations on Python
 Copyright 2022 by Artem Ustsov
 """
 
-import time
-
-from matrixes import matrixes_cpython
-from matrixes import matrixes_python
-
-
-NUMBER = 10
-start = time.time()
-matrixes_python.multiply_matrix_chain_command(
-    chain="100,200,1000,1,5,10,11", is_random="True",
-)
-end = time.time()
-
-py_time = end - start
-print(f"Python time = {py_time}")
-
-start = time.time()
-matrixes_cpython.test(NUMBER)
-end = time.time()
-
-cy_time = end - start
-print(f"Cython time = {cy_time}")
-
-print(f"Speedup = {py_time / cy_time}")
+# import time
+#
+# from matrixes import matrixes_cpython
+# from matrixes import matrixes_ctypes
+#
+#
+# NUMBER = 10
+# start = time.time()
+# matrixes_python.multiply_matrix_chain_command(
+#     chain="100,200,1000,1,5,10,11", is_random="True",
+# )
+# end = time.time()
+#
+# py_time = end - start
+# print(f"Python time = {py_time}")
+#
+# start = time.time()
+# matrixes_cpython.test(NUMBER)
+# end = time.time()
+#
+# cy_time = end - start
+# print(f"Cython time = {cy_time}")
+#
+# print(f"Speedup = {py_time / cy_time}")
