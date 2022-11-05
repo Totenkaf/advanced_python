4670b6def2e5157ac5c09aca26b229fce2bfdb8e
Totenkaf <uscov.1998@mail.ru>
2022-11-05 04:00:11 +0300

    perf test complete

--- 07/matrixes/matrixes_ctypes.py
+++ 07/matrixes/matrixes_ctypes.py
@@ -6,12 +6,12 @@ Copyright 2022 by Artem Ustsov
 
 import ctypes
 from typing import List
-lib1 = ctypes.CDLL('matrix_chain_multiplication.so')
-lib1.sum.multiply_matrix_chain_command = (ctypes.POINTER(ctypes.c_int), ctypes.c_int, ctypes.c_int)
+lib1 = ctypes.CDLL('matrixes/matrix_chain_multiplication.so')
+lib1.multiply_matrix_chain_command.argtypes = (ctypes.POINTER(ctypes.c_int), ctypes.c_int, ctypes.c_int)
 
 
-def multiply_matrix_chain_command(arr: List[int], edge: int) -> int:
+def multiply_matrix_chain_command(arr: List[int], edge: int):
     arr_len = len(arr)
     arr_type = ctypes.c_int * arr_len
-    result = lib1.sum(arr_type(*arr), ctypes.c_int(arr_len), ctypes.c_int(edge))
+    result = lib1.multiply_matrix_chain_command(arr_type(*arr), ctypes.c_int(arr_len), ctypes.c_int(edge))
     return result
--- 07/matrixes/tests/perf/pert_test.py
+++ 07/matrixes/tests/perf/pert_test.py
@@ -1,30 +1,36 @@
 """
 Matrix chain miltiplication.
-Calculations on Python
+Executing programs time comparison test
 Copyright 2022 by Artem Ustsov
 """
 
-# import time
-#
-# from matrixes import matrixes_cpython
-# from matrixes import matrixes_ctypes
-#
-#
-# NUMBER = 10
-# start = time.time()
-# matrixes_python.multiply_matrix_chain_command(
-#     chain="100,200,1000,1,5,10,11", is_random="True",
-# )
-# end = time.time()
-#
-# py_time = end - start
-# print(f"Python time = {py_time}")
-#
-# start = time.time()
-# matrixes_cpython.test(NUMBER)
-# end = time.time()
-#
-# cy_time = end - start
-# print(f"Cython time = {cy_time}")
-#
-# print(f"Speedup = {py_time / cy_time}")
+import random
+import time
+
+from matrixes import matrixes_python
+from matrixes import matrixes_ctypes
+
+
+
+if __name__ == "__main__":
+    start = time.time()
+    matrix_chain = ','.join([str(random.randint(0, 10)) for item in range(1000)])
+
+
+    matrixes_python.multiply_matrix_chain_command(
+        chain=matrix_chain, is_random="True",
+    )
+    end = time.time()
+
+    py_time = end - start
+    print(f"Python time = {py_time}")
+
+    EDGE = 5
+    start = time.time()
+    matrixes_ctypes.multiply_matrix_chain_command(matrix_chain, EDGE)
+    end = time.time()
+
+    cy_time = end - start
+    print(f"Ctypes time = {cy_time}")
+
+    print(f"Speedup = {py_time / cy_time}")
